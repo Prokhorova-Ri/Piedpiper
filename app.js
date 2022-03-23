dbf1246dd6b4262b1bee0e20db91d5f03a6d3062
Prokhorova-ri <rena2652@gmail.com>
2022-03-23 23:08:38 +0300

    Added scroll to the top of the page when clicking on the logo. Added scroll navigation.

--- app.js
+++ app.js
@@ -1,19 +1,64 @@
-$(function() {
 
+/* Fixed header */
+$(function() {
+  
   let header = $("#header");
   let section = $("#section"); 
-  let sectionH;
+  let sectionH = section.innerHeight();
   let scrollPos = $(window).scrollTop();
 
-  $(window).on("scroll load resize", function() {
+  checkSkroll(scrollPos, sectionH);
+
+  $(window).on("scroll resize", function() {
     sectionH = section.innerHeight();
-    scrollPos = $(this).scrollTop();
-    
+    scrollPos = $(this).scrollTop();    
+    checkSkroll(scrollPos, sectionH); 
+  });
+
+  function checkSkroll(scrollPos, sectionH) {
     if( scrollPos > sectionH ) {
       header.addClass("fixed");
     } else {
       header.removeClass("fixed");
     }
+  }
 
+  /* Smooth scroll */
+
+  $("[data-scroll]").on("click", function(event){
+    event.preventDefault();
+
+    let elenentId = $(this).data('scroll');
+    let elementOffset = $(elenentId).offset().top;
+
+    nav.removeClass("show");
+
+    $("html, body").animate({
+      scrollTop: elementOffset - 60
+    }, 800);    
   });
 });
+
+/* NavToggle */
+
+let nav = $("#nav");
+let navToggle = $("#navToggle");
+
+  navToggle.on("click", function(event){
+    event.preventDefault();
+    nav.toggleClass("show");
+});
+
+/* Scroll on top */
+$(function() {
+  $(window).scroll(function() {
+    if($(this).scrollTop() != 0) {
+
+    $('#button').fadeIn();
+  } 
+  });
+
+      $('#button').click(function() {
+      $('body,html').animate({scrollTop:0},800); 
+     });
+  });
